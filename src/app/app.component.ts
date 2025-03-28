d66031db916a2160e3cb0bb53f2886b8e97a3746
Denis Almeida <denis.silva.ext@sascar.com.br>
2025-03-28 12:16:48 -0300

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TimeCalculatorComponent } from './components/time-calculator/time-calculator.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TimeCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/time-calculator/time-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-calculator',
+  imports: [],
+  templateUrl: './time-calculator.component.html',
+  styleUrl: './time-calculator.component.css'
+})
+export class TimeCalculatorComponent {
+
+}
